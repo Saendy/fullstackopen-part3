4afa602b24ff05c596355dac80c93d5218cb6c9f
Saendy <jayden_saw@hotmail.com>
2023-07-12 11:59:00 +0800

    Exercises 3.1-3.8

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+
+var morgan = require('morgan')
+
+const app = express()
+
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+
+app.use(morgan('tiny', {
+  skip: function (req, res) { return req.method === "POST" }
+}))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
+  skip: function (req, res) { return req.method !== "POST" }
+}))
+
+
+
+app.use(express.json())
+
+let phonebook = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has infor for ${phonebook.length} people</p><p>${Date()}</p>`)
+})
+  
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const data = phonebook.find(person => person.id === id)
+    if(data) {
+        response.json(data)
+    }
+    else {
+        response.status(404).end()
+    }
+  })
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phonebook = phonebook.filter(person => person.id !== id)
+    response.status(204).end()
+  })
+
+  app.post('/api/persons', (request, response) => {
+    let body = request.body
+    if (body.name && body.number ){
+        if(phonebook.find(person => person.name === body.name)) {
+            return response.status(400).json({ 
+                error: 'name must be unique' 
+              })
+        }
+        else{
+            newId = Math.floor(Math.random() * 9999999999)
+            phonebook.push({"id":newId, "name":body.name, "number":body.number})
+            body.id = newId
+        }
+    }
+    else {
+        return response.status(400).json({ 
+            error: 'request must contain name and number' 
+          })
+    }
+    response.json(body)
+  })
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
